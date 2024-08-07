bdaa492f45b6bd903af617f43403eee6b43c1955
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2024-08-07 22:28:17 +0900

    init vitest

---
+++ vitest.setup.ts
@@ -0,0 +1,15 @@
+import { beforeAll } from 'vitest';
+
+beforeAll(() => {
+  global.ResizeObserver = class ResizeObserver {
+    observe() {
+      // do nothing
+    }
+    unobserve() {
+      // do nothing
+    }
+    disconnect() {
+      // do nothing
+    }
+  };
+});
