4b3dcdb310af88f8ffb29946d38e7e3c322918a3
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2024-08-08 14:17:58 +0900

    ci(github): deploy to pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: process.env.REPOSITORY_NAME,
+  images: {
+    unoptimized: true,
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
